9aa6e282b67ac2c000e030106e6a9e8b8f724d91
Jesus Rios <etxcoding@gmail.com>
2020-04-23 23:22:39 -0700

    multine, destruct, spread op, let and const

--- ECMAScript6/src/es6/index.js
+++ ECMAScript6/src/es6/index.js
@@ -1,3 +1,4 @@
+// DEFAULT PARAMS
 function newFunction(name, age, country) {
 	var name = name || 'Jesus';
 	var age = age || 29;
@@ -12,13 +13,63 @@ function newFunction2(name = 'Jesus', age = 29, country = 'MX') {
 
 newFunction2();
 newFunction2('Ricardo', 30, 'CO');
+// DEFAULT PARAMS
 
+// TEMPLATE LITERALS
 let hello = 'Hello';
 let world = 'World';
 
 let epichPhrase = hello + ' ' + world;
-
-console.log(epichPhrase);
-
 let epichPhrase2 = `${hello} ${world}`;
+console.log(epichPhrase);
 console.log(epichPhrase2);
+// TEMPLATE LITERALS
+
+// MULTILINE
+let lorem = 'qwer tyui op. asdfgh jkl. \n' + 'zxcv bn m.';
+let lorem2 = `qwer tyui op. asdfgh jkl.
+zxcv bn m.
+`;
+
+console.log(lorem);
+console.log(lorem2);
+// MULTILINE
+
+// DESTRUCT
+let person = {
+	name: 'Jesus',
+	age: 29,
+	country: 'MX'
+};
+console.log(person.name, person.age);
+
+let { name, age, country } = person;
+console.log(name, age, country);
+// DESTRUCT
+
+// SPREAD OPERATOR
+let team1 = ['Jesus', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Jessica', 'Ana'];
+
+let education = ['David', ...team1, ...team2];
+console.log(education);
+// SPREAD OPERATOR
+
+// LET
+{
+	var global = 'Global var';
+}
+
+{
+	let globalLet = 'Global let';
+	console.log(globalLet);
+}
+
+console.log(global);
+// LET
+
+// CONST
+const a = 'b';
+// a = 'a'; // error
+console.log(a);
+// CONST
