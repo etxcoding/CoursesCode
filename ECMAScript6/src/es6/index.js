57364b8bcb74359af7f9608737ec51df9aad07f0
Jesus Rios <etxcoding@gmail.com>
2020-04-23 23:57:55 -0700

    classes, modules and generators.

--- ECMAScript6/src/es6/index.js
+++ ECMAScript6/src/es6/index.js
@@ -122,3 +122,46 @@ helloPromise()
 	.then(() => console.log('hola3'));
 
 // PROMISES
+
+// CLASSES
+
+class Calculator {
+	constructor() {
+		this.valueA = 0;
+		this.valueB = 0;
+	}
+
+	sum(valueA, valueB) {
+		this.valueA = valueA;
+		this.valueB = valueB;
+
+		return this.valueA + this.valueB;
+	}
+}
+
+const calc = new Calculator();
+console.log(calc.sum(2, 2));
+// CLASSES
+
+// MODULES
+import { hello } from './module';
+hello();
+// MODULES
+
+// GENERATORS
+
+function* helloWorld() {
+	if (true) {
+		yield 'Hello, ';
+	}
+	if (true) {
+		yield 'World';
+	}
+}
+
+const generateHello = helloWorld();
+console.log(generateHello.next().value);
+console.log(generateHello.next().value);
+console.log(generateHello.next().value);
+
+// GENERATORS
