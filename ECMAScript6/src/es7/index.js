01c624d6e70a7c266c38917e73c18c343cde6138
Jesus Rios <etxcoding@gmail.com>
2020-04-25 08:35:52 -0700

    es7 includes and power

---
+++ ECMAScript6/src/es7/index.js
@@ -0,0 +1,16 @@
+// INCLUDES
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(9)) {
+	console.log('7 has been found!');
+} else {
+	console.log('Not found');
+}
+// INCLUDES
+
+// POWER
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+console.log(result);
+// POWER
