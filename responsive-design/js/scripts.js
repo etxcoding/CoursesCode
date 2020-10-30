42659bcaf4dc10e3ac123222387db05ed2b0f71f
Jesus Rios <jesusriosdev@gmail.com>
2020-10-29 17:06:30 -0700

    burger menu event listener on click to show menu or remove it, listener based on media querys with js

--- responsive-design/js/scripts.js
+++ responsive-design/js/scripts.js
@@ -1,9 +1,26 @@
+const ipad = window.matchMedia("screen and (max-width: 767px)");
+
 const burgetButton = document.querySelector("#burger-menu");
 const menu = document.querySelector(".menu");
 
-burgetButton.addEventListener("click", () => {
-	hideShow();
-});
+// if (ipad.matches)
+// 	burgetButton.addEventListener("click", () => {
+// 		hideShow();
+// 	});
+
+ipad.addListener(validation);
+
+function validation(event) {
+	if (event.matches) {
+		burgetButton.addEventListener("click", () => {
+			hideShow();
+		});
+	} else {
+		burgetButton.removeEventListener("click", () => {
+			hideShow();
+		});
+	}
+}
 
 function hideShow() {
 	menu.classList.toggle("is-active");
